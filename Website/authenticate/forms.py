61f1fcca4241e1bffbf6682410d6294d77b78936
Valérie <samuelpilcer@gmail.com>
2017-10-23 23:52:21 +0200

    Change signature form

--- Website/authenticate/forms.py
+++ Website/authenticate/forms.py
@@ -2,7 +2,9 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from ..articles.models import Signature
+import sys
+sys.path.append("../")
+from articles.models import Signature
 
 
 class ConnexionForm(forms.Form):
