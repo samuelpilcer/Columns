382e32751a03515e5084210954c49c6819c66914
Valérie <samuelpilcer@gmail.com>
2017-11-01 21:31:36 +0100

    Ranking process

--- Website/articles/process_ranking.py
+++ Website/articles/process_ranking.py
@@ -9,13 +9,13 @@ def process_ranking_all():
 		publication=article.date
 		now = datetime.now()
 		duration=now-publication.replace(tzinfo=None)
-		article.ranking=(article.likes/10+1)*exp(-duration.days/7)
+		article.ranking=1000*(article.likes*0.1+1)*exp(-duration.days/7)
 		article.save()
 
 def process_ranking(id):
 	article = Article.objects.get(id=id)
 	publication=article.date
 	now = datetime.now()
-	duration=now-publication
-	article.ranking=(article.likes/10+1)*exp(-duration.days/7)
+	duration=now-publication.replace(tzinfo=None)
+	article.ranking=1000*(article.likes*0.1+1)*exp(-duration.days/7)
 	article.save()
