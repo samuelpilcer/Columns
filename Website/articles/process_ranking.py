4cffcb5da5f8334dd128eaaed2d05964385a4af4
Valérie <samuelpilcer@gmail.com>
2017-11-01 21:20:27 +0100

    Ranking process

--- Website/articles/process_ranking.py
+++ Website/articles/process_ranking.py
@@ -1,9 +1,16 @@
 from articles.models import Article
-from datetime import date
+from datetime import date, datetime
 from numpy import exp
 
 def process_ranking_all():
 	articles = Article.objects.all()
+	for i in range(articles):
+		article=articles[i]
+		publication=article.date
+		now = datetime.now()
+		duration=now-publication.replace(tzinfo=None)
+		article.ranking=(article.likes/10+1)*exp(-duration.days/7)
+		article.save()
 
 def process_ranking(id):
 	article = Article.objects.get(id=id)
