0d27b36c543e579eb6c32e525cc8a558f7dcb1dc
Valérie <samuelpilcer@gmail.com>
2017-11-02 19:44:45 +0100

    Fix bug

--- Website/Website/urls.py
+++ Website/Website/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from articles import views as core_views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^$', core_views.home, name='home'),
@@ -25,3 +27,5 @@ urlpatterns = [
     url(r'^oauth/', include('social_django.urls', namespace='social')),
     url(r'^oauth/', include('social.apps.django_app.urls', namespace='social')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
