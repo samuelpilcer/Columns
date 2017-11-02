cbf5fe93255b40dc469030b1b1c83292cdf22f89
Valérie <samuelpilcer@gmail.com>
2017-11-02 11:35:30 +0100

    Test Form Article

--- Website/Website/settings.py
+++ Website/Website/settings.py
@@ -69,7 +69,7 @@ MIDDLEWARE_CLASSES = [
 
 ROOT_URLCONF = 'Website.urls'
 
-MEDIUM_EDITOR_THEME = 'bootstrap'
+MEDIUM_EDITOR_THEME = 'roman'
 MEDIUM_EDITOR_OPTIONS = {
     'toolbar': {
         'static': False,
