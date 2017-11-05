dbc17acb2f517ab95e3d80deb81dd50042f65765
Valérie <samuelpilcer@gmail.com>
2017-11-05 21:14:54 +0100

    Undo changes

--- Website/Website/settings.py
+++ Website/Website/settings.py
@@ -34,8 +34,8 @@ ALLOWED_HOSTS = ['*']
 LOGIN_URL = '/authenticate/connexion'
 #SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 #SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+#SESSION_COOKIE_SECURE = True
+#CSRF_COOKIE_SECURE = True
 
 # Application definition
 
