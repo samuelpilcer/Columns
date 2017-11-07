70d3148c0bc5c9139387b85ac5332e894b6ebd05
Valérie <samuelpilcer@gmail.com>
2017-11-07 16:21:47 +0100

    Test fix bug url

--- Website/articles/urls.py
+++ Website/articles/urls.py
@@ -15,5 +15,5 @@ urlpatterns = [
     url(r'^article/save/(\d+)$', views.save, name='save'),
     url(r'^article/new$', views.new, name='new'),
     url(r'^article/actualize_rank$', views.actualize_rank, name='actualize_rank'),
-    url(r'^user/(?P<user>[\w\-]+)/(?P<article>[\w\-]+)$', views.get_article, name='get_article'),
+    url(r'^user/(?P<url>[\w\-]+)$', views.get_article, name='get_article'),
 ]
--- Website/articles/views.py
+++ Website/articles/views.py
@@ -18,7 +18,7 @@ def get_id(url):
     return url.split('-')[-1]
 
 def get_url(article):
-    url='user/'
+    url=''
     url=url+article.auteur.username+'/'
     url=url+unicodedata.normalize('NFKD',  article.titre.lower()).encode('ascii', 'ignore').replace(',','').replace(';','').replace('/','').replace(':','').replace("'",'-').replace("\"",'').replace(' ','-')+'-'
     url=url+str(article.id)
@@ -185,13 +185,11 @@ def search_page(request, phrase, nb):
 def read_by_tag_p0(request, id):
     return read_by_tag(request, id, 0)
 
-def get_article(request, user, article):
-    id=get_id(article)
+def get_article(request, url):
+    id=get_id(url)
     try:
         article = Article.objects.get(id=id)
     except Article.DoesNotExist:
-        raise Http404
-    if article.auteur.username!=user:
         raise Http404
     else:
         return lire(request,id)
