ef0c0b6bce19954eabf5da7f1f23e32fd92c4252
Valérie <samuelpilcer@gmail.com>
2017-11-27 20:29:50 +0100

    Modify url

--- Website/articles/views.py
+++ Website/articles/views.py
@@ -20,7 +20,7 @@ def get_id(url):
 def get_url(article):
     url='user/'
     url=url+article.auteur.username.lower()+'/'
-    url=url+unicodedata.normalize('NFKD',  article.titre.lower()).encode('ascii', 'ignore').replace(',','').replace('#','').replace('(','').replace(')','').replace(';','').replace('/','').replace(':','').replace("'",'-').replace("\"",'').replace(' ','-')+'-'
+    url=url+unicodedata.normalize('NFKD',  article.titre.lower()).encode('ascii', 'ignore').replace(',','').replace('?','').replace('!','').replace('(','').replace(')','').replace('$','').replace('#','').replace('@','').replace('%','').replace('#','').replace('(','').replace(')','').replace(';','').replace('/','').replace(':','').replace("'",'-').replace("\"",'').replace(' ','-')+'-'
     url=url+str(article.id)
     return url
 
