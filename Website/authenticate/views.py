c9cc9d5dc7ef247af93da439f72e06a8bc8c668a
Valérie <samuelpilcer@gmail.com>
2018-01-08 23:46:50 +0100

    Fix bug

--- Website/authenticate/views.py
+++ Website/authenticate/views.py
@@ -66,7 +66,7 @@ def inscription(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            UserData(user).save()
+            UserData(user=user).save()
             return redirect('connexion')
     else:
         form = InscriptionForm()
