c0cbbc60f63550cc077d20058c2e54e102126f7c
Valérie <samuelpilcer@gmail.com>
2018-01-10 13:23:27 +0100

    Test

--- Website/articles/views.py
+++ Website/articles/views.py
@@ -551,6 +551,74 @@ def metrics(request, id):
     else:
         return redirect(reverse(home))
 
+@login_required
+def promote(request, id):
+    try:
+        article = Article.objects.get(id=id)
+    except Article.DoesNotExist:
+        raise Http404
+
+    if article.auteur==request.user:
+        try:
+            likes = Like.objects.all().filter(article=article)
+            number_of_likes=len(likes)
+        except:
+            number_of_likes=0
+        
+        try:
+            comments=Comment.objects.all().filter(article=article)
+            nb_comments=len(comments)
+        except:
+            comments=[]
+            nb_comments=0
+
+        analytics = initialize_analyticsreporting()
+        report=get_report(analytics)
+        personal_report_dim=[]
+        personal_report=[]
+        temps_moyen=0
+        sources={}
+        medium={}
+        cities={}
+        for i in range(len(report['reports'][0]['data']['rows'])):
+            if get_id(report['reports'][0]['data']['rows'][i]['dimensions'][5])==id:
+                personal_report_dim.append(report['reports'][0]['data']['rows'][i]['dimensions'])
+                personal_report.append(report['reports'][0]['data']['rows'][i]['metrics'][0]['values'])
+                temps_moyen=temps_moyen+float(report['reports'][0]['data']['rows'][i]['metrics'][0]['values'][3])
+                if report['reports'][0]['data']['rows'][i]['dimensions'][1] not in sources:
+                    sources[report['reports'][0]['data']['rows'][i]['dimensions'][1]]=1
+                else:
+                    sources[report['reports'][0]['data']['rows'][i]['dimensions'][1]]=sources[report['reports'][0]['data']['rows'][i]['dimensions'][1]]+1
+                if report['reports'][0]['data']['rows'][i]['dimensions'][2] not in medium:
+                    medium[report['reports'][0]['data']['rows'][i]['dimensions'][2]]=1
+                else:
+                    medium[report['reports'][0]['data']['rows'][i]['dimensions'][2]]=medium[report['reports'][0]['data']['rows'][i]['dimensions'][2]]+1
+                if report['reports'][0]['data']['rows'][i]['dimensions'][4] not in cities:
+                    cities[report['reports'][0]['data']['rows'][i]['dimensions'][4]]=1
+                else:
+                    cities[report['reports'][0]['data']['rows'][i]['dimensions'][4]]=cities[report['reports'][0]['data']['rows'][i]['dimensions'][4]]+1
+        if (len(personal_report)>0):
+            temps_moyen=int(temps_moyen/len(personal_report))
+        else:
+            temps_moyen=0
+        table_sources=[]
+        for i in sources:
+            table_sources.append(table_row(i,sources[i]))
+        table_medium=[]
+        for i in medium:
+            table_medium.append(table_row(i,medium[i]))
+        table_cities=[]
+        for i in cities:
+            table_cities.append(table_row(i,cities[i]))
+        fils=InFil.objects.all().filter(article=article)
+        in_fils=[]
+        for i in fils:
+            in_fils.append(i.fil)
+        form=AddToFilForm(None)
+        return render(request, 'blog/analytics.html', {'article': article,'series':in_fils,'fil_form':form,'table_medium': table_medium, 'table_cities':table_cities, 'table_sources': table_sources, 'report':personal_report, 'vues':len(personal_report), 'temps_moyen':temps_moyen, 'report_dim': personal_report_dim, 'number_of_comments':nb_comments, 'number_of_likes':number_of_likes})
+    else:
+        return redirect(reverse(home))
+
 
 @login_required
 def new(request):
