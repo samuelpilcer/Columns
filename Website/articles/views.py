516ae069eb659458164d38dabe90b2d9a5df2cc9
Valérie <samuelpilcer@gmail.com>
2018-01-10 17:10:20 +0100

    New way to display charts

--- Website/articles/views.py
+++ Website/articles/views.py
@@ -620,6 +620,7 @@ def metrics(request, id):
                 else:
                     date_table.append(chart_point(str(i.day)+"-"+str(i.month)+"-"+str(i.year),0))
             dates_vues_table="["
+            print(dates_vues_table)
             for i in date_table:
                 dates_vues_table=dates_vues_table+"'"+i.date+"',"
             dates_vues_table=dates_vues_table[:-1]+"]"
