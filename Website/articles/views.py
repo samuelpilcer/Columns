f5624963c92216e7ffe01ce6b9f344bf134d2a50
Valérie <samuelpilcer@gmail.com>
2018-01-10 17:35:57 +0100

    New way to display charts

--- Website/articles/views.py
+++ Website/articles/views.py
@@ -646,6 +646,10 @@ def metrics(request, id):
         return redirect(reverse(home))
 
 @login_required
+def promote_p0(request):
+    return promote(request,0)
+
+@login_required
 def promote(request, id):
     try:
         article = Article.objects.get(id=id)
